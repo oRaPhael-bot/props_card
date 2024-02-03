28ada1428026999d9d5554f92ec266414fa36d29
Raphael <raphamaia9@gmail.com>
2024-02-03 16:19:58 -0300

    feat: add example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './Card'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Adicione três elementos de cartão.</h1>
+      <Card h2 = "Primeiro cartão h2" h3 = "Primeiro cartão h3"/>
+      <Card h2 = "Segundo cartão h2" h3 = "Segundo cartão h3"/>
+      <Card h2 = "Terceiro cartão h2" h3 = "Terceiro cartão h3"/>
     </div>
   );
-}
+};
 
 export default App;
